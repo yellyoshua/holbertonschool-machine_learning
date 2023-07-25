68464b531ae647090fbe701a0c6ca44989db980e
yellyoshua <usuario1529yoshua@hotmail.com>
2023-07-24 21:55:41 -0500

    Change doc message

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 def matrix_shape(matrix):
-    """Calculates the shape of a matrix"""
+    """OK"""
     shape = []
     while type(matrix) == list:
         shape.append(len(matrix))
