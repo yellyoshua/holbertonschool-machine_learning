0f6c138b5cfae75d2ca2c26e6b9b4adbd1e4e8aa
yellyoshua <usuario1529yoshua@hotmail.com>
2023-07-26 22:11:07 -0500

    add print numpy transpose func

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def np_transpose(matrix):
+    """ transpose of a matrix
+
+    Args:
+        matrix: Given matrix
+
+    Return:
+        transposed matrix
+    """
+    return matrix.transpose()
